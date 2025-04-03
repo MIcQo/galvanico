965c4ca24902c391b79a28ca2da222ef6cefbd7b
MIcQo <micqogamer@gmail.com>
2025-04-03 20:54:00 +0200

    feat(migration): rename ships to train cars
    Signed-off-by: MIcQo <micqogamer@gmail.com>

--- migrations/20250329234725_create_user_resources_table.up.sql
+++ migrations/20250329234725_create_user_resources_table.up.sql
@@ -1,18 +1,18 @@
 CREATE TABLE "user_resources"
 (
-    "user_id"         uuid PRIMARY KEY NOT NULL,
-    "gold"            float            NOT NULL DEFAULT 0,
-    "ships"           integer          NOT NULL DEFAULT 0,
-    "available_ships" integer          NOT NULL DEFAULT 0,
-    "electricity"     float            NOT NULL DEFAULT 0,
-    "max_electricity" float            NOT NULL DEFAULT 100,
-    "waste"           float            NOT NULL DEFAULT 0,
-    "max_waste"       float            NOT NULL DEFAULT 100,
-    "updated_at"      timestamp        NOT NULL DEFAULT now()
+    "user_id"              uuid PRIMARY KEY NOT NULL,
+    "gold"                 integer          NOT NULL DEFAULT 10000,
+    "train_cars"           integer          NOT NULL DEFAULT 0,
+    "available_train_cars" integer          NOT NULL DEFAULT 0,
+    "electricity"          float            NOT NULL DEFAULT 0,
+    "max_electricity"      float            NOT NULL DEFAULT 100,
+    "waste"                float            NOT NULL DEFAULT 0,
+    "max_waste"            float            NOT NULL DEFAULT 100,
+    "updated_at"           timestamp        NOT NULL DEFAULT now()
 );
 
 ALTER TABLE "user_resources"
     ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE cascade ON UPDATE no action;
 
-insert into user_resources (user_id, gold, ships, available_ships)
-values ('c522b16b-a157-410d-8a3e-6fc64d84c17d', 1000, 1, 1);
+insert into user_resources (user_id)
+values ('c522b16b-a157-410d-8a3e-6fc64d84c17d');
