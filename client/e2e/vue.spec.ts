4df1683dff80946c7e1c4b26753dd12c4706894a
MIcQo <micqogamer@gmail.com>
2025-04-10 00:18:36 +0200

    test(e2e): #50 add auth tests
    Signed-off-by: MIcQo <micqogamer@gmail.com>

--- client/e2e/vue.spec.ts
+++ client/e2e/vue.spec.ts
@@ -4,5 +4,17 @@ import {expect, test} from '@playwright/test';
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({page}) => {
   await page.goto('/');
-  await expect(page.locator('h1')).toHaveText('You did it!');
+  await expect(page.locator('h1')).toHaveText('Galvanico');
+})
+
+test('visit register page', async ({page}) => {
+  await page.goto('/auth/register');
+  await expect(page.locator('h2')).toHaveText('Register');
+  await expect(page.locator('.btn.btn-primary.w-full')).toHaveText('Register');
+})
+
+test('visit login page', async ({page}) => {
+  await page.goto('/auth/login');
+  await expect(page.locator('h2')).toHaveText('Login');
+  await expect(page.locator('.btn.btn-primary.w-full')).toHaveText('Login');
 })
