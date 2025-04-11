842e1293e26c7ae258ac7a8236b8a9e078178d19
MIcQo <micqogamer@gmail.com>
2025-04-11 19:58:42 +0200

    style(auth): cleanup
    Signed-off-by: MIcQo <micqogamer@gmail.com>

--- client/src/views/Auth/Login.vue
+++ client/src/views/Auth/Login.vue
@@ -34,14 +34,14 @@ const login = async () => {
   const user = await defaultInstance.post('auth/login', opts).json()
     .catch(async (r) => {
       loading.value = false;
-    if (r.response.status >= 500) {
-      alert.open(t('global.errors.errorOccurred'), AlertType.danger)
-    } else {
-      const errorData = await r.response.json();
-      alert.open(t(`auth.responses.${errorData.message}`), AlertType.warning)
-    }
-    return null;
-  }) as tokenResponse | null;
+      if (r.response.status >= 500) {
+        alert.open(t('global.errors.errorOccurred'), AlertType.danger)
+      } else {
+        const errorData = await r.response.json();
+        alert.open(t(`auth.responses.${errorData.message}`), AlertType.warning)
+      }
+      return null;
+    }) as tokenResponse | null;
 
   loading.value = false;
   if (user && user.token) {
@@ -54,41 +54,41 @@ const login = async () => {
 </script>
 
 <template>
-  <form action="">
-  <h2 class="card-title">{{ $t('auth.login') }}</h2>
-  <div class="items-center mt-2">
-    <label class="w-full input input-bordered flex items-center gap-2 mb-2">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-           class="w-4 h-4 opacity-70">
-        <path
-          d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
-        <path
-          d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
-      </svg>
-      <input v-model="email" type="text" class="grow" :placeholder="$t('auth.fields.email')"/>
-    </label>
-    <label class="w-full input input-bordered flex items-center gap-2 mb-2">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-           class="w-4 h-4 opacity-70">
-        <path fill-rule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clip-rule="evenodd"/>
-      </svg>
-      <input v-model="password" type="password" class="grow"
-             :placeholder="$t('auth.fields.password')" value=""/>
-    </label>
-  </div>
-  <div class="card-actions justify-center">
-    <!-- TODO: in the future   <a href="#" class="text-center">Forgot password?</a>-->
-    <button type="submit" :disabled="loading || !email || !password" @click.prevent="login"
-            class="btn btn-primary w-full">
-      <span v-if="loading" class="loading loading-spinner"></span>
-      {{ $t('auth.login') }}
-    </button>
-    <RouterLink :to="{name: 'auth.register'}" class="text-center">
-      {{ $t('auth.doesNotHaveAccount') }}
-    </RouterLink>
-  </div>
+  <form action="" @keyup.enter.prevent="login">
+    <h2 class="card-title">{{ $t('auth.login') }}</h2>
+    <div class="items-center mt-2">
+      <label class="w-full input input-bordered flex items-center gap-2 mb-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+             class="w-4 h-4 opacity-70">
+          <path
+            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+          <path
+            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+        </svg>
+        <input v-model="email" type="email" class="grow" :placeholder="$t('auth.fields.email')"/>
+      </label>
+      <label class="w-full input input-bordered flex items-center gap-2 mb-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+             class="w-4 h-4 opacity-70">
+          <path fill-rule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clip-rule="evenodd"/>
+        </svg>
+        <input v-model="password" type="password" class="grow"
+               :placeholder="$t('auth.fields.password')" value=""/>
+      </label>
+    </div>
+    <div class="card-actions justify-center">
+      <!-- TODO: in the future   <a href="#" class="text-center">Forgot password?</a>-->
+      <button type="submit" :disabled="loading || !email || !password" @click.prevent="login"
+              class="btn btn-primary w-full">
+        <span v-if="loading" class="loading loading-spinner"></span>
+        {{ $t('auth.login') }}
+      </button>
+      <RouterLink :to="{name: 'auth.register'}" class="text-center">
+        {{ $t('auth.doesNotHaveAccount') }}
+      </RouterLink>
+    </div>
   </form>
 </template>
 
--- client/src/views/Auth/Register.vue
+++ client/src/views/Auth/Register.vue
@@ -48,69 +48,72 @@ const register = async (e: Event) => {
 
   loading.value = true;
 
-  await defaultInstance.post('auth/register', opts).json().catch(async (r) => {
+  const response = await defaultInstance.post('auth/register', opts).json().catch(async (r) => {
     loading.value = false;
     if (r.response.status >= 500) {
       alert.open(t('global.errors.errorOccurred'), AlertType.danger)
     } else {
       const errorData = await r.response.json();
-      console.log(errorData);
       alert.open(t(`auth.responses.${errorData.message}`), AlertType.warning)
     }
+
+    return null;
   });
 
-  alert.open(t('auth.alert.successRegister'), AlertType.success)
   loading.value = false;
-  await router.push({name: 'auth.login'})
+  if (response) {
+    alert.open(t('auth.alert.successRegister'), AlertType.success)
+    await router.push({name: 'auth.login'});
+  }
 }
 </script>
 
 <template>
   <form>
-  <h2 class="card-title">{{ $t("auth.register") }}</h2>
-  <div class="items-center mt-2">
-    <label class="w-full input input-bordered flex items-center gap-2 mb-2">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-           class="w-4 h-4 opacity-70">
-        <path
-          d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
-        <path
-          d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
-      </svg>
-      <input type="text" class="grow" v-model="email" :placeholder="$t('auth.fields.email')"/>
-    </label>
-    <label class="w-full input input-bordered flex items-center gap-2 mb-2">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-           class="w-4 h-4 opacity-70">
-        <path fill-rule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clip-rule="evenodd"/>
-      </svg>
-      <input type="password" class="grow" v-model="password"
-             :placeholder="$t('auth.fields.password')" value=""/>
-    </label>
-    <label class="w-full input input-bordered flex items-center gap-2 mb-2">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-           class="w-4 h-4 opacity-70">
-        <path fill-rule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clip-rule="evenodd"/>
-      </svg>
-      <input type="password" class="grow" v-model="confirmPassword"
-             :placeholder="$t('auth.fields.confirmPassword')"
-             value=""/>
-    </label>
-  </div>
-  <div class="card-actions justify-center">
-    <button type="submit" :disabled="loading || !email || !password || !confirmPassword"
-            @click.prevent="register"
-            class="btn btn-primary w-full">
-      <span v-if="loading" class="loading loading-spinner"></span>
-      {{ $t("auth.register") }}
-    </button>
-    <RouterLink :to="{name: 'auth.login'}" class="text-center">{{ $t("auth.alreadyHaveAccount") }}
-    </RouterLink>
-  </div>
+    <h2 class="card-title">{{ $t("auth.register") }}</h2>
+    <div class="items-center mt-2">
+      <label class="w-full input input-bordered flex items-center gap-2 mb-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+             class="w-4 h-4 opacity-70">
+          <path
+            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+          <path
+            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+        </svg>
+        <input type="text" class="grow" v-model="email" :placeholder="$t('auth.fields.email')"/>
+      </label>
+      <label class="w-full input input-bordered flex items-center gap-2 mb-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+             class="w-4 h-4 opacity-70">
+          <path fill-rule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clip-rule="evenodd"/>
+        </svg>
+        <input type="password" class="grow" v-model="password"
+               :placeholder="$t('auth.fields.password')" value=""/>
+      </label>
+      <label class="w-full input input-bordered flex items-center gap-2 mb-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+             class="w-4 h-4 opacity-70">
+          <path fill-rule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clip-rule="evenodd"/>
+        </svg>
+        <input type="password" class="grow" v-model="confirmPassword"
+               :placeholder="$t('auth.fields.confirmPassword')"
+               value=""/>
+      </label>
+    </div>
+    <div class="card-actions justify-center">
+      <button type="submit" :disabled="loading || !email || !password || !confirmPassword"
+              @click.prevent="register"
+              class="btn btn-primary w-full">
+        <span v-if="loading" class="loading loading-spinner"></span>
+        {{ $t("auth.register") }}
+      </button>
+      <RouterLink :to="{name: 'auth.login'}" class="text-center">{{ $t("auth.alreadyHaveAccount") }}
+      </RouterLink>
+    </div>
   </form>
 </template>
 
