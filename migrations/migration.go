be7eb735f04e85298651dda185cf66a8b5d02f2c
MIcQo <micqogamer@gmail.com>
2025-04-11 23:26:51 +0200

    fix(chore): docker build was built without sh, use embed for migrations
    Signed-off-by: MIcQo <micqogamer@gmail.com>

--- migrations/migration.go
+++ migrations/migration.go
@@ -1,11 +1,17 @@
 package migrations
 
-import "github.com/uptrace/bun/migrate"
+import (
+	"embed"
+	"github.com/uptrace/bun/migrate"
+)
 
 var Migrations = migrate.NewMigrations()
 
+//go:embed sql
+var migs embed.FS
+
 func init() {
-	if err := Migrations.DiscoverCaller(); err != nil {
+	if err := Migrations.Discover(migs); err != nil {
 		panic(err)
 	}
 }
